700b53710db4a6a8d117a31b1e316a23a1ab83f3
Rishabh Garg <narishabh.garg@gmail.com>
2025-06-25 16:58:12 +0530

    Created Basic Character Setup

---
+++ Source/BasicShooter/Public/Characters/ShooterCharacter.h
@@ -0,0 +1,40 @@
+// Basic Shooter
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class BASICSHOOTER_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AShooterCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComponent;
+
+private:
+	void MoveForward(float AxisValue);
+	void LookUp(float AxisValue);
+	void MoveRight(float AxisValue);
+	void LookRight(float AxisValue);
+
+	UPROPERTY(EditAnywhere)
+	float RotationRate = 70.f;
+
+};
